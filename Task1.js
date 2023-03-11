79573ec3828808b1c6589db30f64966e5dc295ff
shahrukh88 <66013821+shahrukh88@users.noreply.github.com>
2023-03-11 12:22:21 +0500

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -8,19 +8,21 @@ function find_longest_word(str)
     if(result.length < array1[x].length)
     {
     result = array1[x];
+
+       
     } 
     else  if(result.length == array1[x].length)
     {
         const result2 =array1[x]; 
         const count = result.match(/[aeiou]/gi).length;
+        
         const count2 = result2.match(/[aeiou]/gi).length;
-          
-        if(count>count2){
+         if(count>count2){
             result = array1[x];
-
-        }
+          }
         else if(count< count2){
             result = result2;
+         
         }
    }        
         
@@ -28,7 +30,8 @@ function find_longest_word(str)
   }
   return result;
 }
-console.log (find_longest_word('Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers'));
+
+console.log(find_longest_word('Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers'));
 
 
 
